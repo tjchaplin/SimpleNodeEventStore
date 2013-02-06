85fb7957fcc55683fe63b29873830fb5adc1c334
Tim Chaplin <tjchaplin@hotmail.com>
2013-02-06 23:09:41 +0000

    Refactored to get the index

--- Configuration.js
+++ Configuration.js
@@ -4,7 +4,7 @@ module.exports = function(app,express) {
     app.configure(function(){
         app.set('port', process.env.PORT || 3000);
         app.set('view engine', 'dust');
-        app.set('views', path.join(__dirname,  '/views'));
+        app.set('views', app.root+'/views');
 
         app.use(app.router);
 
@@ -15,11 +15,7 @@ module.exports = function(app,express) {
         app.use(express.cookieParser());
         app.use(express.session({ secret: 'very_unique_secret_string',
             cookie: { maxAge: 1800000 }}));
-        app.use(express.static(path.join(__dirname, 'public'), {redirect: false}));
-    });
-
-    app.configure('development', function(){
-        app.use(express.errorHandler());
+        app.use(express.static(app.root+'/public', {redirect: false}));
     });
 
 }
--- Server.js
+++ Server.js
@@ -3,24 +3,25 @@ var express = require('express')
     , routes = require('./Server')
     , http = require('http')
     , path = require('path')
-    , dust = require('dust');
+    , cons = require('consolidate')
+    , dustln = require('dustjs-linkedin')
+    ,package = require('./package.json');;
 
 var app = express();
 
-app.engine('dust', dust);
+app.engine('dust', cons.dust);
+
 app.root = __dirname;
+app.set("name",package.name+" ("+package.version+")");
+
 global.host = 'localhost';
 
-app.get('/', routes.index);
+console.log("Package:"+package.name+" app.name:"+app.name);
 
-require('Configuration')(app,express);
+require('./configuration')(app,express);
 require('./server/router')(app,express);
 
 
-app.listen(app.port, function(){
-    console.log("Express server listening on port:"+app.port);
+app.listen(app.get('port'), function(){
+    console.log("Express server listening on port:"+app.get('port'));
 });
-
-//http.createServer(app).listen(app.get('port'), function(){
-//  console.log("Express server listening on port " + app.get('port'));
-//});
